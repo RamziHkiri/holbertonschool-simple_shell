20111c7954d1655217f3ed325d6bc29482ab2ad1
RamziHkiri <6257@holbertonstudents.com>
2023-04-25 13:54:45 -0700

    fix betty style

--- split_line.c
+++ split_line.c
@@ -2,27 +2,34 @@
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 /**
+ * split_line - split the commande
+ * @line: the commande to split
+ * Return: double pointer string
  */
 char **split_line(char *line)
 {
 	int bufsize = LSH_TOK_BUFSIZE, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
+	char **tokens = malloc(bufsize * sizeof(char *));
 	char *token;
 
-	if (!tokens) {
+	if (!tokens)
+	{
 		fprintf(stderr, "lsh: allocation error\n");
 		exit(EXIT_FAILURE);
 	}
 
 	token = strtok(line, LSH_TOK_DELIM);
-	while (token != NULL) {
+	while (token != NULL)
+	{
 		tokens[position] = token;
 		position++;
 
-		if (position >= bufsize) {
+		if (position >= bufsize)
+		{
 			bufsize += LSH_TOK_BUFSIZE;
-			tokens = realloc(tokens, bufsize * sizeof(char*));
-			if (!tokens) {
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
 				fprintf(stderr, "lsh: allocation error\n");
 				exit(EXIT_FAILURE);
 			}
@@ -31,5 +38,5 @@ char **split_line(char *line)
 		token = strtok(NULL, LSH_TOK_DELIM);
 	}
 	tokens[position] = NULL;
-	return tokens;
+	return (tokens);
 }
