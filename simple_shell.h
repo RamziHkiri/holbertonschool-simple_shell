5238ea30e673caa3f1fd9e16b418b8acc6ca2795
RamziHkiri <6257@holbertonstudents.com>
2023-04-28 05:50:41 -0700

    header file for the  simple shell project

--- simple_shell.h
+++ simple_shell.h
@@ -12,8 +12,28 @@
 #include<signal.h>
 #include <sys/wait.h>
 #include <stddef.h>
+/**
+ * struct builtins - builtins function's struct
+ * @commande:commande
+ * @func: pointer function
+ */
+typedef struct builtins
+{
+	char *commande;
+	void (*func)(char **cmd);
+} builtins_t;
+
+extern char **environ;
+
+void _free_cmd(char **token);
+void get_path(char **cmd);
+void free_array(char **array);
 char *read_line(void);
 void inf_loop(void);
 void execute_cmd(char **cmd);
 char **split_line(char *line);
+void print_env(void);
+void sh_cd(char **cmd);
+void sh_help(char **cmd);
+void sh_exit(char **cmd);
 #endif
